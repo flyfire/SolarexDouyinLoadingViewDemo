0bc53770487cbe5c5488b9fc007f5d23d20c72cb
Solarex <rh.hou.work@gmail.com>
2018-10-16 16:01:45 +0800

    init project

---
+++ SolarexDouyinLoadingView/src/main/java/com/solarexsoft/solarexdouyinloadingview/SolarexDouyinLoadingView.java
@@ -0,0 +1,28 @@
+package com.solarexsoft.solarexdouyinloadingview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * <pre>
+ *    Author: houruhou
+ *    CreatAt: 15:55/2018/10/16
+ *    Desc:
+ * </pre>
+ */
+public class SolarexDouyinLoadingView extends View {
+    public SolarexDouyinLoadingView(Context context) {
+        this(context, null);
+    }
+
+    public SolarexDouyinLoadingView(Context context, @androidx.annotation.Nullable AttributeSet
+            attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SolarexDouyinLoadingView(Context context, @androidx.annotation.Nullable AttributeSet
+            attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
